840ebab1ab0b2aa16bd7b7f70617c51d762ae41f
thisLight <l1589002388@gmail.com>
2021-09-20 22:48:47 +0800

    update build script for sam3 library

--- sam3/build.zig
+++ sam3/build.zig
@@ -24,6 +24,7 @@ pub fn build(b: *std.build.Builder) void {
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
+    const target = b.standardTargetOptions(.{});
 
     const CCFLAGS = [_][]const u8{"-Wall", "-g", "-std=gnu99"};
     const LIBSAM3_INCLUDE = "./libsam3/src/libsam3/";
@@ -32,17 +33,11 @@ pub fn build(b: *std.build.Builder) void {
     var mainPackage = autopkg.accept(package("sam3", "."));
     defer mainPackage.deinit();
     var resolvedPackage = mainPackage.resolve(".", b.allocator) catch unreachable;
+    
     const lib = resolvedPackage.addBuild(b);
     lib.setBuildMode(mode);
     lib.install();
 
-    var main_tests = b.addTest("src/sam3.zig");
-    main_tests.addIncludeDir(LIBSAM3_INCLUDE);
-    main_tests.addCSourceFile(LIBSAM3_SRC, CCFLAGS[0..2]);
-    main_tests.addPackage(.{.name="strings", .path="strings/strings.zig"});
-    main_tests.linkLibC();
-    main_tests.setBuildMode(mode);
-
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
+    test_step.dependOn(resolvedPackage.addTest(b, mode, &target));
 }
