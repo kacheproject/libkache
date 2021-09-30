e9380052b142b3f9436752f017fc57178feafe63
thisLight <l1589002388@gmail.com>
2021-09-30 09:23:58 +0800

    build: update script with testSrcs & sqlite: update to kache owned upstream dynamic-stmt

--- build.zig
+++ build.zig
@@ -2,17 +2,25 @@ const std = @import("std");
 const autopkg = @import("autopkg/autopkg.zig");
 
 pub fn package(name: []const u8, path: []const u8) autopkg.AutoPkgI {
+    return selfPackage(name, path, true);
+}
+
+fn selfPackage(name: []const u8, path: []const u8, skipTest: bool) autopkg.AutoPkgI {
     var sam3 = @import("sam3/build.zig").package("sam3", "sam3");
-    var sqlite = @import("pkgs/sqlite.zig").package("sqlite", "pkgs/sqlite");
+    const sqlite = @import("pkgs/sqlite/build.zig");
     return autopkg.genExport(autopkg.AutoPkg{
         .name = name,
         .path = path,
         .rootSrc = "src/kache.zig",
         .dependencies = &.{
             autopkg.accept(sam3),
-            autopkg.accept(sqlite),
+            autopkg.accept(sqlite.package("sqlite", "pkgs/sqlite", .{})),
         },
         .linkLibC = true,
+        .doNotTest = skipTest,
+        .testSrcs = &.{
+            "src/profile.zig",
+        },
     });
 }
 
@@ -20,17 +28,18 @@ pub fn build(b: *std.build.Builder) void {
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
+    const target = b.standardTargetOptions(.{});
 
-    var mainPackage = autopkg.accept(package("kache", "."));
+    var mainPackage = autopkg.accept(selfPackage("kache", ".", false));
     defer mainPackage.deinit();
     var resolvedPackage = mainPackage.resolve(".", b.allocator) catch unreachable;
+
     const lib = resolvedPackage.addBuild(b);
     lib.setBuildMode(mode);
     lib.install();
 
-    var main_tests = b.addTest("src/kache.zig");
-    main_tests.setBuildMode(mode);
+    var packages_tests = resolvedPackage.addTest(b, mode, &target);
 
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
+    test_step.dependOn(packages_tests);
 }
