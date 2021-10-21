06c684d98c2b196818db990b841391a5fcaf08d9
thisLight <l1589002388@gmail.com>
2021-10-21 12:33:53 +0800

    build: add zmq

--- build.zig
+++ build.zig
@@ -8,6 +8,7 @@ pub fn package(name: []const u8, path: []const u8) autopkg.AutoPkgI {
 fn selfPackage(name: []const u8, path: []const u8, skipTest: bool) autopkg.AutoPkgI {
     var sam3 = @import("sam3/build.zig").package("sam3", "sam3");
     const sqlite = @import("pkgs/sqlite/build.zig");
+    const zmq = @import("pkgs/zmq/build.zig");
     return autopkg.genExport(autopkg.AutoPkg{
         .name = name,
         .path = path,
@@ -15,6 +16,7 @@ fn selfPackage(name: []const u8, path: []const u8, skipTest: bool) autopkg.AutoP
         .dependencies = &.{
             autopkg.accept(sam3),
             autopkg.accept(sqlite.package("sqlite", "pkgs/sqlite", .{})),
+            autopkg.accept(zmq.package("zmq", "pkgs/zmq", .{})),
         },
         .linkLibC = true,
         .doNotTest = skipTest,
